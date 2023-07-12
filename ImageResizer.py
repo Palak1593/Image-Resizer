7bb2cd0c1c365d217e74fdc48292b7c86a58c10d
PALAK SINGLA <108184072+Palak1593@users.noreply.github.com>
2023-07-12 15:49:33 +0530

    Update ImageResizer.py

--- ImageResizer.py
+++ ImageResizer.py
@@ -1,24 +1,4 @@
-# import cv2
-# image=cv2.imread("images.jpeg",cv2.IMREAD_UNCHANGED)
-# # cv2.imshow("title",image)
-# # cv2.waitKey(0)
 
-# #percent by which the image is resized 
-# scale_percent=50
-
-# #calculate the 50 percent of original dimanesions
-# new_width=int(image.shape[1] *scale_percent /100)
-
-# new_height =int(image.shape[0] *scale_percent /100) 
-
-# dsize=(new_width, new_height) 
-
-# #resize image
-# output=cv2.resize(image, dsize)
-
-# cv2.imwrite('new_image.png',output)  
-
-# cv2.waitKey(0)     
 
 import cv2
 
